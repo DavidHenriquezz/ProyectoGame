dfb03c34b26f83ebe9fa2775bd96ff2046c09790
BenjaminSepulvedaO <163463275+BenjaminSepulvedaO@users.noreply.github.com>
2024-10-31 23:03:29 -0300

    Update PowerUp.java

--- SpaceNav2024-core/src/main/java/puppy/code/PowerUp.java
+++ SpaceNav2024-core/src/main/java/puppy/code/PowerUp.java
@@ -1,10 +1,12 @@
 package puppy.code;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public interface PowerUp {
 	void aplicarEfecto(Nave4 nave); // Aplica el efecto del power-up a la nave
 	void update(); // Actualiza la posición y el estado del power-up
 	void render(SpriteBatch batch); // Dibuja el power-up en pantalla
 	boolean isCollected(); // Verifica si el power-up fue recogido por la nave
+	public Rectangle getBounds();
 }
